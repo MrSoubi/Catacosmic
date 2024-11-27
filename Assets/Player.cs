7f5ef02fb908ef76931a487e7ae9e319e0717c26
Furnok <killian.furno@orange.fr>
2024-11-27 14:24:23 +0100

    Feat: Joystick

---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public JoyStick joyStick;
+
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if(joyStick.joystickVec.y != 0)
+        {
+            rb.linearVelocity = new Vector2(joyStick.joystickVec.x * speed, joyStick.joystickVec.y * speed);
+        }
+        else
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
+    }
+}
