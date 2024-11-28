2848091c3459ca159b1a8eacb22b97a57330ecff
Furnok <killian.furno@orange.fr>
2024-11-28 07:59:27 +0100

    Fix: Click Movement Bug on UI

--- Assets/Scripts/PointerClick.cs
+++ Assets/Scripts/PointerClick.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PointerClick : MonoBehaviour
 {
@@ -11,9 +12,14 @@ public class PointerClick : MonoBehaviour
         target = transform.position;
     }
 
+    private bool IsPointerOverUIObject()
+    {
+        return EventSystem.current.IsPointerOverGameObject();
+    }
+
     private void FixedUpdate()
     {
-        if(Input.GetMouseButton(0))
+        if(Input.GetMouseButton(0) && !IsPointerOverUIObject())
         {
             target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             target.z = transform.position.z;
