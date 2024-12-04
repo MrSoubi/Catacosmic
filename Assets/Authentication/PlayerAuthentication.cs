8385930468124e430868f6e0c40aab0dfdfe77e1
EmmanuelBouis <emmanuel.bouis@outlook.fr>
2024-12-04 10:25:55 +0100

    Fix Authentication bug

--- Assets/Authentication/PlayerAuthentication.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-using Unity.Services.Authentication;
-using Unity.Services.Authentication.PlayerAccounts;
-using Unity.Services.Core;
-using UnityEngine;
-
-public class PlayerAuthentication : MonoBehaviour
-{
-}
