9698481d0dd1ee09bd77a4614699e2c16907c082
Furnok <killian.furno@orange.fr>
2024-12-06 00:01:49 +0100

    Feat: Controller V2, but Keep Controller V1 because of Bugs

--- Assets/Scripts/FreePressV2.cs
+++ Assets/Scripts/FreePressV2.cs
@@ -1,53 +1,242 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
 
 public class FreePressV2 : MonoBehaviour
 {
-    public RSE_PointerDown test;
-    public RSO_PointerPosition test2;
-    public RSE_PointerUp test3;
+    [Header("ScriptableObjects")]
+    [SerializeField] private MapInfos mapInfos;
+    [SerializeField] private RSE_PointerDown test;
+    [SerializeField] private RSO_PointerPosition test2;
+    [SerializeField] private RSE_PointerUp test3;
 
-    public float moveSpeed;
+    [Header("Zoom")]
+    [SerializeField] private float speedCam;
+    [SerializeField] private float zoomMax;
+    [SerializeField] private float zoomMin;
+    [SerializeField] private float speedZoomCam;
+    [SerializeField] private float smoothTime;
+    [SerializeField] private float zoomChangeMin;
 
-    private Vector2 touchPress;
-    private Vector2 touchPressCurrent;
+    [Header("Camera Circle")]
+    [SerializeField] private Transform circleCamera;
 
-    private bool isTouchPress;
+    [Header("Cinemachine")]
+    [SerializeField] private CinemachineCamera cinemachineCamera;
+    [SerializeField] private CinemachineConfiner2D cinemachineConfiner;
+
+    private Camera mainCamera;
+
+    private Vector3 velocity;
+    private Vector3 touchPress;
+    private Vector3 freePressTouch;
+    private Vector3 freePressTouchPos;
+    private Vector3 freePressDist;
+    private Vector3 touch1Press;
+    private Vector3 touch2Press;
+
+    private Vector2 boundsSize;
+
+    private bool isPressed;
+    private bool isDecelerating;
+    private bool isTouchPress1;
+    private bool isZooming;
+
+    private Coroutine moveCoroutine;
+    private Coroutine deceleratingCoroutine;
+    private Coroutine zoomCoroutine;
 
     private void Awake()
     {
-        test.Fire += TouchPress;
-        test2.onValueChanged += TouchMove;
+        test.Fire += TouchDown;
+        test2.onValueChanged += TouchPos;
         test3.Fire += TouchUp;
     }
 
-    private void TouchPress()
+    private void Start()
     {
-        // Store the initial touch position when the user presses on the screen
-        touchPress = test2.Value;  // Assuming test2 gives the current touch position
-        isTouchPress = true;
+        mainCamera = Camera.main;
+
+        cinemachineConfiner.BoundingShape2D = mapInfos.BoxCollider2DRef;
+
+        Collider2D confinerCollider = cinemachineConfiner.BoundingShape2D;
+
+        if (confinerCollider != null)
+        {
+            boundsSize.x = confinerCollider.GetComponent<SpriteRenderer>().bounds.size.x;
+            boundsSize.y = confinerCollider.GetComponent<SpriteRenderer>().bounds.size.y;
+        }
     }
 
-    private void TouchMove(Vector2 currentPosition)
+    /// <summary>
+    /// Lock the Camera inside the Border
+    /// </summary>
+    /// <param name="newPosition"></param>
+    /// <returns></returns>
+    private Vector3 LockToCameraBorder(Vector3 newPosition)
     {
-        if (!isTouchPress) return;
+        Collider2D confinerCollider = cinemachineConfiner.BoundingShape2D;
 
-        // Calculate the movement direction based on touch movement
-        touchPressCurrent = currentPosition;
+        if (confinerCollider != null)
+        {
+            Vector2 confinerBounds = boundsSize;
+            Vector2 confinerCenter = confinerCollider.bounds.center;
 
-        // Calculate the difference in position
-        Vector2 delta = touchPressCurrent - touchPress;
+            Vector2 val = mapInfos.PlayerRef.transform.GetChild(0).GetComponent<SpriteRenderer>().bounds.size / 2;
 
-        // Move the camera in the opposite direction of the touch movement
-        // Adjust the camera's position based on the delta
-        Camera.main.transform.position -= new Vector3(delta.x, 0, delta.y) * moveSpeed * Time.deltaTime;
+            float minX = confinerCenter.x - confinerBounds.x / 2 + val.x;
+            float maxX = confinerCenter.x + confinerBounds.x / 2 - val.x;
+            float minY = confinerCenter.y - confinerBounds.y / 2 + val.y;
+            float maxY = confinerCenter.y + confinerBounds.y / 2 - val.y;
 
-        // Update the touchPress to the current position for smooth continuous movement
-        touchPress = currentPosition;
+            newPosition.x = Mathf.Clamp(newPosition.x, minX, maxX);
+            newPosition.y = Mathf.Clamp(newPosition.y, minY, maxY);
+
+            return newPosition;
+        }
+        else
+        {
+            return newPosition;
+        }
     }
 
-    private void TouchUp()
+    /// <summary>
+    /// Move the Camera
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator CameraMove()
     {
-        // Reset the touch press state when the touch ends
-        isTouchPress = false;
+        yield return null;
+
+        freePressTouchPos = mainCamera.ScreenToWorldPoint(touchPress);
+        freePressTouchPos.z = -10;
+
+        while (!isZooming && (isPressed))
+        {
+            Vector3 currentTouchPos = mainCamera.ScreenToWorldPoint(touchPress);
+            currentTouchPos.z = -10;
+
+            freePressDist = freePressTouchPos - currentTouchPos;
+            freePressDist.z = -10;
+
+            Vector3 targetPosition = transform.position + freePressDist;
+
+            targetPosition = LockToCameraBorder(targetPosition);
+
+            targetPosition.z = -10;
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, speedCam * Time.deltaTime);
+
+            mapInfos.CameraTransform = transform.position;
+
+            yield return null;
+        }
+    }
+
+    /// <summary>
+    /// Deceleration of the Camera on UnTouch
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator Deceleration()
+    {
+        while (isDecelerating && !isZooming)
+        {
+            Vector3 currentTouchPos = mainCamera.ScreenToWorldPoint(freePressTouch);
+            currentTouchPos.z = -10;
+
+            freePressDist = freePressTouchPos - currentTouchPos;
+            freePressDist.z = -10;
+
+            Vector3 targetPosition = transform.position + freePressDist;
+
+            targetPosition = LockToCameraBorder(targetPosition);
+            targetPosition.z = -10;
+
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
+            mapInfos.CameraTransform = transform.position;
+
+            if (velocity.magnitude < 0.1f)
+            {
+                isDecelerating = false;
+
+                velocity = Vector3.zero;
+                freePressTouchPos = Vector3.zero;
+                freePressDist = Vector3.zero;
+            }
+
+            yield return null;
+        }
+    }
+
+
+    /// <summary>
+    /// Touch Position
+    /// </summary>
+    /// <param name="ctx"></param>
+    public void TouchPos(Vector2 pos)
+    {
+        touchPress = pos;
+        touchPress.z = -10;
+    }
+
+    /// <summary>
+    /// Touch Down
+    /// </summary>
+    public void TouchDown()
+    {
+        isPressed = true;
+        isDecelerating = false;
+
+        if (moveCoroutine != null)
+        {
+            StopCoroutine(moveCoroutine);
+            moveCoroutine = null;
+        }
+
+        if (deceleratingCoroutine != null)
+        {
+            StopCoroutine(deceleratingCoroutine);
+            deceleratingCoroutine = null;
+        }
+
+        velocity = Vector3.zero;
+        freePressTouchPos = Vector3.zero;
+        freePressDist = Vector3.zero;
+
+        freePressTouch = touchPress;
+        freePressTouch.z = -10;
+
+        if (moveCoroutine == null)
+        {
+            moveCoroutine = StartCoroutine(CameraMove());
+        }
+    }
+
+    /// <summary>
+    /// Touch Up
+    /// </summary>
+    public void TouchUp()
+    {
+        isPressed = false;
+
+        isDecelerating = true;
+
+        if (deceleratingCoroutine != null)
+        {
+            StopCoroutine(deceleratingCoroutine);
+            deceleratingCoroutine = null;
+        }
+
+        freePressTouch = touchPress;
+
+        if (deceleratingCoroutine == null)
+        {
+            deceleratingCoroutine = StartCoroutine(Deceleration());
+        }
     }
 }
