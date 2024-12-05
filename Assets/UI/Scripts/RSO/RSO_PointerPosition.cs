c70fba037978f69a7518039cacce0d069dedb82c
EmmanuelBouis <emmanuel.bouis@outlook.fr>
2024-12-05 23:34:05 +0100

    Fix position axis from UI Toolkit

--- Assets/UI/Scripts/RSO/RSO_PointerPosition.cs
+++ Assets/UI/Scripts/RSO/RSO_PointerPosition.cs
@@ -9,22 +9,25 @@ using UnityEngine.Events;
 public class RSO_PointerPosition : ScriptableObject
 {
     public Action<Vector2> onValueChanged;
-    public string position;
 
     [ShowInInspector]
     private Vector2 _value;
+
+    public Vector2 position;
 
     public Vector2 Value
     {
         get => _value;
         set
         {
-            if (_value == value) return;
+            if (_value.x == value.x && _value.y == Screen.height - value.y) return;
 
-            _value = value;
+            _value.x = value.x;
+            _value.y = Screen.height - value.y;
+
+            position = _value;
+
             onValueChanged?.Invoke(_value);
-
-            position = _value.x + " / " + _value.y;
         }
     }
 }
