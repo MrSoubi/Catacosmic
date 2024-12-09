32a149dda77024f543227d8426bba6d4601e8450
Furnok <killian.furno@orange.fr>
2024-12-09 08:36:59 +0100

    Fix

--- Assets/Scripts/Controllers/RSO_DisasterPosition.cs
+++ Assets/Scripts/Controllers/RSO_DisasterPosition.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
-using UnityEngine.Events;
 
 [CreateAssetMenu(fileName = "RSO_DisasterPosition", menuName = "Data/RSO/DisasterPosition")]
 public class RSO_DisasterPosition : ScriptableObject
